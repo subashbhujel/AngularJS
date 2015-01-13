945c10914fb9362c02dc55ab4e810286158fbcc2
Subash Bhujel <subasb@microsoft.com>
2015-01-13 13:55:04 -0800

    Adding TODO app

---
+++ js/ToDo.js
@@ -0,0 +1,28 @@
+﻿function todoCtrl($scope) {    
+
+    // returns the total number of TODO list
+    $scope.getTotalTodos = function () {
+        return $scope.todos.length;
+    }
+
+    // Array of ToDo list
+    $scope.todos = [
+        { text: 'Go get RayBan', done: false },
+        { text: 'Workout', done: false },
+        { text: 'Open an Chase Account', done: false },
+        { text: 'Indoor Soccer', done: false }
+    ]
+
+    // Add new todo List
+    $scope.addTodo = function () {
+        $scope.todos.push({ text: $scope.newTodo, done: false });
+        $scope.newTodo = '';
+    }
+
+    //Removes the todo List
+    $scope.removeTodo = function () {
+        $scope.todos = _.filter($scope.todos, function (todo) {
+            return !todo.done;
+        })
+    }
+}
