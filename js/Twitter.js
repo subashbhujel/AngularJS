9603ad5b2c32a9408849935ac03cb7323025c4e1
Subash Bhujel <subasb@microsoft.com>
2015-01-13 16:48:28 -0800

    Adding Twitter Search app

---
+++ js/Twitter.js
@@ -0,0 +1,12 @@
+﻿angular.module('Twitter', ['ngResource']);
+
+function twitterCtrl($scope, $resource) {
+   
+   $scope.twitter = $resource('http://search.twitter.com/:action',
+        { action: 'search.json', q: 'angularjs', callback: 'JSON_CALLBACK' },
+        { get: { method: 'JSONP' } });
+
+    $scope.doSearch = function () {
+        $scope.twitterResult = $scope.twitter.get({ q: $scope.searchTerm });
+    };
+}
